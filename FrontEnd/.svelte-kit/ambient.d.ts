969f0ca512c531086e5c29861b9ab42ecea5ac61
Nayeon Shin <contact@nayeonshin.dev>
2023-03-21 17:56:43 -0400

    feat(`+page.svelte`): extract HTML URLs to Gists into an array

--- FrontEnd/.svelte-kit/ambient.d.ts
+++ FrontEnd/.svelte-kit/ambient.d.ts
@@ -39,8 +39,7 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const FPS_BROWSER_APP_PROFILE_STRING: string;
-	export const FPS_BROWSER_USER_PROFILE_STRING: string;
+	export const EFC_7652: string;
 	export const HOME: string;
 	export const HOMEDRIVE: string;
 	export const HOMEPATH: string;
@@ -143,8 +142,7 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		FPS_BROWSER_APP_PROFILE_STRING: string;
-		FPS_BROWSER_USER_PROFILE_STRING: string;
+		EFC_7652: string;
 		HOME: string;
 		HOMEDRIVE: string;
 		HOMEPATH: string;
--- FrontEnd/src/routes/newPage/+page.svelte
+++ FrontEnd/src/routes/newPage/+page.svelte
@@ -1,17 +1,25 @@
 <script>
-    import { fade, scale } from "svelte/transition";
-    let show = false;
+  import { fade, scale } from "svelte/transition";
+  let show = false;
 
-    export let data;
-    console.log(data); // TODO: remove console.log
-  </script>
-  
-  <label>
-    Toggle
-    <input type="checkbox" bind:checked={show} />
-  </label>
-  <hr />
-  {#if show}
-    <h1 in:scale out:fade>My name is Forest Code</h1>
-  {/if}
-  
+  export let data;
+
+  const { data: gists } = data;
+  const urls = [];
+
+  for (const gist of gists) {
+    const { html_url: url } = gist;
+    urls.push(url);
+  }
+
+  console.log(urls); // TODO: remove console.log
+</script>
+
+<label>
+  Toggle
+  <input type="checkbox" bind:checked={show} />
+</label>
+<hr />
+{#if show}
+  <h1 in:scale out:fade>My name is Forest Code</h1>
+{/if}
