9efa9639fb2c91ef1f767a414758e489263e78ea
Nayeon Shin <contact@nayeonshin.dev>
2023-05-07 17:49:18 -0400

    fix: show one apple on button click

--- FrontEnd/src/routes/newPage/+page.svelte
+++ FrontEnd/src/routes/newPage/+page.svelte
@@ -53,14 +53,136 @@
       />
     </a>
   {/each} -->
-  <a href = {`/paper/?id=${ids[0]}`}>
-    <img src = {apple} alt = "apple"  class = "absolute  w-[10vw] top-[2vh] lg:w-[7vw] left-[44vw] lg:top-[0%] lg:left-[46%] hover:w-[8vw]"/>
+  <a href={`/paper/?id=${ids[0]}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] top-[2vh] lg:w-[7vw] left-[44vw] lg:top-[0%] lg:left-[46%] hover:w-[8vw]"
+    />
   </a>
-  <a href = {`/paper/?id=${ids[1]}`}>
-    <img src = {apple} alt = "apple" class = "absolute w-[10vw] top-[7%] lg:w-[7vw] left-[50%] lg:top-[6%]  hover:w-[8vw]"/>
+  <a href={`/paper/?id=${ids[1]}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute w-[10vw] top-[7%] lg:w-[7vw] left-[50%] lg:top-[6%]  hover:w-[8vw]"
+    />
   </a>
-  <a href = {`/paper/?id=${ids[2]}`}>
-    <img src = {apple} alt = "apple"class = "absolute w-[10vw] top-[7%] lg:w-[7vw] lg:left-[43%] left-[40%] lg:top-[6%]  hover:w-[8vw]"/>
+  <a href={`/paper/?id=${ids[2]}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute w-[10vw] top-[7%] lg:w-[7vw] lg:left-[43%] left-[40%] lg:top-[6%]  hover:w-[8vw]"
+    />
+  </a>
+
+  <a href="/paper" class={`${count > 1 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute w-[10vw] top-[7%] lg:w-[7vw] lg:left-[43%] left-[40%] lg:top-[6%]  hover:w-[8vw]"
+    />
+  </a>
+
+  <!-- ROW 3 -->
+  <a href="/paper" class={`${count > 2 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute w-[10vw] lg:w-[7vw] top-[15vh] left-[55%] hover:w-[8vw]"
+    />
+  </a>
+  <a href="/paper" class={`${count > 3 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] lg:w-[7vw] top-[15vh] lg:left-[46%] left-[45%] hover:w-[8vw]"
+    />
+  </a>
+  <a href="/paper" class={`${count > 4 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] lg:w-[7vw] top-[15vh] lg:left-[38%] left-[35%] hover:w-[8vw]"
+    />
+  </a>
+
+  <!-- ROW 4 -->
+  <a href="/paper" class={`${count > 5 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] lg:w-[7vw] top-[23vh] lg:left-[51%] left-[50%] hover:w-[8vw]"
+    />
+  </a>
+  <a href="/paper" class={`${count > 6 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] lg:w-[7vw] top-[23vh] lg:left-[42%] left-[40%] hover:w-[8vw]"
+    />
+  </a>
+
+  <!-- ROW 5-->
+  <a href="/paper" class={`${count > 7 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] lg:w-[7vw] top-[32vh] left-[55%] hover:w-[8vw]"
+    />
+  </a>
+
+  <a href="/paper" class={`${count > 8 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] lg:w-[7vw] top-[32vh] lg:left-[46%] left-[45%] hover:w-[8vw]"
+    />
+  </a>
+  <a href="/paper" class={`${count > 9 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] lg:w-[7vw] top-[32vh] lg:left-[38%] left-[35%] hover:w-[8vw]"
+    />
+  </a>
+
+  <!-- ROW 6-->
+  <a href="/paper" class={`${count > 10 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] lg:w-[7vw] top-[40vh] lg:left-[51%] left-[50%] hover:w-[8vw]"
+    />
+  </a>
+  <a href="/paper" class={`${count > 11 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] lg:w-[7vw] top-[40vh] lg:left-[42%] left-[40%] hover:w-[8vw]"
+    />
+  </a>
+
+  <!-- ROW 7-->
+  <a href="/paper" class={`${count > 12 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] lg:w-[7vw] top-[48vh] left-[55%] hover:w-[8vw]"
+    />
+  </a>
+  <a href="/paper" class={`${count > 13 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] lg:w-[7vw] top-[48vh] lg:left-[46%] left-[45%] hover:w-[8vw]"
+    />
+  </a>
+  <a href="/paper" class={`${count > 14 ? "visible" : "hidden"}`}>
+    <img
+      src={apple}
+      alt="apple"
+      class="absolute  w-[10vw] lg:w-[7vw] top-[48vh] lg:left-[38%] left-[35%] hover:w-[8vw]"
+    />
   </a>
 
   <img
